d2c7aa41dbe57d2fc3d57ee75aa074dc274075b1
lukmay <lukas.mayr00@gmail.com>
2024-10-30 18:04:47 +0100

    update reuse header

--- app/encryptconfig.sh
+++ app/encryptconfig.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+#
+# SPDX-License-Identifier: CC0-1.0
+
 read -sp 'Password: ' password
 gpg --batch -c -o- --passphrase "$password" config.yaml | base64 > config.yaml.enc
